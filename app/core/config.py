98e821346316ebe2a8dfb263283426efa273b5b5
Nika Babych <nanoornika@gmail.com>
2024-05-08 22:29:24 +0300

    make requested changes

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,16 @@
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 
 class AppConfig:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    PORT = os.getenv("PORT")
+    def __init__(self):
+        self.SECRET_KEY = None
+        self.PORT = None
+        self.HOST = None
+        env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                '../../.env')
+        with open(env_path, "r") as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith("#"):  # Ignore empty lines and comments
+                    key, value = line.split("=")
+                    setattr(self, key, value)
--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 from routers import health_check
 from core.config import AppConfig
 
-secret_key = AppConfig.SECRET_KEY
-database_url = AppConfig.DATABASE_URL
-port = AppConfig.PORT
+config = AppConfig()
 
 app = FastAPI()
 
 app.include_router(health_check.router)
 
 if __name__ == "__main__":
+    host = config.HOST
+    port = int(config.PORT)
+    secret_key = config.SECRET_KEY
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host=host, port=port, reload=True)
