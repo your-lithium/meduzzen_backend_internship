b1bd6db0086e61d2f4a3630c827b1472ff5407d5
Nika Babych <nanoornika@gmail.com>
2024-05-19 14:53:59 +0300

    BE-3-add_databases

---
+++ app/db/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import config
+
+
+engine = create_async_engine(config.postgres_url, echo=True, future=True)
+
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
