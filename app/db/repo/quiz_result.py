2ea5c52bd244074ca30bed8fd943db0cc7e7b83e
your-lithium <lithium@knu.ua>
2024-09-25 16:44:35 +0300

    fix function calls in compliance with changes upstream

--- app/db/repo/quiz_result.py
+++ app/db/repo/quiz_result.py
@@ -40,8 +40,8 @@ class QuizResultRepo(BaseRepo[QuizResult]):
         session: AsyncSession = Depends(get_session),
     ) -> list[QuizResult]:
         return await QuizResultRepo.get_all_by_fields(
-            fields=QuizResult.user_id,
-            values=user_id,
+            fields=[QuizResult.user_id],
+            values=[user_id],
             limit=None,
             offset=0,
             session=session,
@@ -53,8 +53,8 @@ class QuizResultRepo(BaseRepo[QuizResult]):
         session: AsyncSession = Depends(get_session),
     ) -> list[QuizResult]:
         return await QuizResultRepo.get_all_by_fields(
-            fields=QuizResult.company_id,
-            values=company_id,
+            fields=[QuizResult.company_id],
+            values=[company_id],
             limit=None,
             offset=0,
             session=session,
@@ -66,16 +66,9 @@ class QuizResultRepo(BaseRepo[QuizResult]):
         company_id: UUID,
         session: AsyncSession = Depends(get_session),
     ) -> list[QuizResult]:
-        fields = [QuizResult.user_id]
-        values = [user_id]
-
-        if company_id is not None:
-            fields.append(QuizResult.company_id)
-            values.append(company_id)
-
         return await QuizResultRepo.get_all_by_fields(
-            fields=fields,
-            values=values,
+            fields=[QuizResult.user_id, QuizResult.company_id],
+            values=[user_id, company_id],
             limit=None,
             offset=0,
             session=session,
