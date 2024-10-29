4a2e763c5117474a2b87778357cf6c822ccdb86b
your-lithium <lithium@knu.ua>
2024-10-29 19:36:25 +0200

    add test coverage

--- tests/test_quiz.py
+++ tests/test_quiz.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -119,3 +120,93 @@ async def test_delete_quiz(
     await client.delete(f"/quizzes/{quiz_id}")
     quiz = await QuizRepo.get_by_id(record_id=quiz_id, session=test_session)
     assert quiz is None
+
+
+@pytest.mark.parametrize("fill_db_with_memberships", [StatusEnum.MEMBER], indirect=True)
+@pytest.mark.asyncio
+async def test_import_create_quiz(
+    fill_db_with_memberships,
+    fill_db_with_companies,
+    client: AsyncClient,
+    test_session: AsyncSession,
+):
+    _, company_id = await get_user_and_company_ids(
+        company_name=payload.test_company_1.name, session=test_session
+    )
+    with open("tests/payload_files/quiz_create.xlsx", "rb") as file:
+        files = {
+            "quiz_table": (
+                "file.xlsx",
+                file,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            ),
+        }
+        response = await client.put(f"/quizzes/{company_id}/import", files=files)
+
+    assert response.status_code == 200
+    quiz = response.json()
+    expected_quiz = {
+        **payload.expected_test_quiz_1,
+        "company_id": company_id,
+    }
+    assert_real_matches_expected(quiz, expected_quiz)
+
+    notifications = await NotificationRepo.get_all_by_fields(
+        fields=[Notification.text],
+        values=[
+            (
+                f"There's a new quiz {quiz['id']} created by company {company_id}. "
+                "You should take it!"
+            )
+        ],
+        session=test_session,
+    )
+    assert notifications != []
+
+
+@pytest.mark.asyncio
+async def test_import_update_quiz(
+    fill_db_with_quizzes, client: AsyncClient, test_session: AsyncSession
+):
+    _, company_id = await get_user_and_company_ids(
+        company_name=payload.test_company_1.name, session=test_session
+    )
+    quiz = await QuizRepo.get_by_fields(
+        fields=[Quiz.name, Quiz.company_id],
+        values=[payload.test_quiz_1.name, company_id],
+        session=test_session,
+    )
+    assert quiz is not None, "Quiz not found"
+    quiz_id = quiz.id
+
+    file_path = "tests/payload_files/quiz_update.xlsx"
+    df = pd.read_excel(
+        file_path,
+        sheet_name="Quiz",
+        header=None,
+        nrows=4,
+        dtype=object,
+    )
+    df.at[0, 1] = quiz_id
+    with pd.ExcelWriter(
+        file_path, engine="openpyxl", mode="a", if_sheet_exists="overlay"
+    ) as writer:
+        df.to_excel(writer, sheet_name="Quiz", index=False, header=False)
+
+    with open(file_path, "rb") as file:
+        files = {
+            "quiz_table": (
+                "file.xlsx",
+                file,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            ),
+        }
+        response = await client.put(f"/quizzes/{company_id}/import", files=files)
+
+    assert response.status_code == 200
+    updated_quiz = response.json()
+    expected_quiz = {
+        **payload.expected_test_quiz_1_update,
+        "company_id": company_id,
+    }
+    assert_real_matches_expected(updated_quiz, expected_quiz)
